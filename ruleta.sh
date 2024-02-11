22036e62baab912eb51aae176c526968ad8261a7
Jordi Valls <jordivallsplasmx@gmail.com>
2024-02-11 21:35:06 +0100

    ruleta

--- ruleta.sh
+++ ruleta.sh
@@ -72,33 +72,31 @@ function martingala(){
                 let play_counter+=1
                 #sleep 1.5
             else
-                if [ "$par_impar" == "impar" ]; then 
-                    if [ "$(($random_number % 2))" -eq 0 ]; then
-                        if [ "$random_number" -eq 0 ]; then
-                            echo "[+] Ha salido el 0, por tanto has perdido "
-                            initial_bet=$(($initial_bet*2))
-                            bad_games+="$random_number "
-                        else
-                            echo "[+] El número que ha salido es par, pierdes!"
-                            initial_bet=$(($initial_bet*2))
-                            bad_games+="$random_number "
-                        fi
+                if [ "$(($random_number % 2))" -eq 0 ]; then
+                    if [ "$random_number" -eq 0 ]; then
+                        echo "[+] Ha salido el 0, por tanto has perdido "
+                        initial_bet=$(($initial_bet*2))
+                        bad_games+="$random_number "
                     else
-                        echo "[+] El número que ha salido es impar, ganas!"
-                        reward=$(($initial_bet*2))
-                        echo "[+] Ganas un total de $reward€"
-                        money=$(($money+$reward))
+                        echo "[+] El número que ha salido es par, pierdes!"
+                        initial_bet=$(($initial_bet*2))
+                        bad_games+="$random_number "
+                    fi
+                else
+                    echo "[+] El número que ha salido es impar, ganas!"
+                    reward=$(($initial_bet*2))
+                    echo "[+] Ganas un total de $reward€"
+                    money=$(($money+$reward))
+                    better_game=$money
+                    if [ $money -gt $better_game ]; then
                         better_game=$money
-                        if [ $money -gt $better_game ]; then
-                            better_game=$money
-                        fi
-                        initial_bet=$backup_bet
-                        bad_games=" "
-                    fi  
-                    echo -e "[+] Tines $money€\n"
-                    let play_counter+=1
-                    #sleep 1
-                fi
+                    fi
+                    initial_bet=$backup_bet
+                    bad_games=" "
+                fi  
+                echo -e "[+] Tines $money€\n"
+                let play_counter+=1
+                #sleep 1
             fi
         else
             # Nos quedamos sin dinero
@@ -233,6 +231,97 @@ function fibonacci() {
     done
 }
 
+function alembert() {
+    echo -e "\n[+] Dinero actual:$money€"
+    echo -ne "[+] ¿Cuánto dinero tines pensado apostar? -> " && read initial_bet
+    echo -ne "[+] ¿A qué deseas apostar continuamente (par/impar)? ->  " && read par_impar
+
+    echo -e "[+] Vamos a jugar con una cantidad inicial de $initial_bet€ a $par_impar\n"
+
+    backup_bet=$initial_bet
+    play_counter=1
+    bad_games=" "
+    better_game=$initial_bet
+
+    while true; do
+        money=$(($money-$initial_bet))
+        echo "[+] Acabas de apostar $initial_bet€ y tines $money€"
+        random_number="$(($RANDOM % 37))"
+        echo -e "[+] Ha salido el número $random_number"
+
+        if [  "$money" -ge 0 ]; then
+            if [ "$par_impar" == "par" ]; then 
+                if [ "$(($random_number % 2))" -eq 0 ]; then
+                    if [ "$random_number" -eq 0 ]; then
+                        echo "[+] Ha salido el 0, por tanto has perdido "
+                        initial_bet=$(($initial_bet+1))
+                        bad_games+="$random_number "
+                    else
+                        echo "[+] El número que ha salido es par, ganas!"
+                        reward=$(($initial_bet*2))
+                        echo "[+] Ganas un total de $reward€"
+                        money=$(($money+$reward))
+                        better_game=$money
+                        if [ $money -gt $better_game ]; then
+                            better_game=$money
+                        fi
+                        if [ $initial_bet == 1 ]; then
+                            initial_bet=1
+                        else    
+                            initial_bet=$(($initial_bet-1))
+                        fi
+                        bad_games=" "
+                    fi
+                else
+                    echo "[+] El número que ha salido es impar, pierdes!"
+                    initial_bet=$(($initial_bet+1))
+                    bad_games+="$random_number "
+                fi
+                echo -e "[+] Tines $money€\n"
+                let play_counter+=1
+                #sleep 1.5
+            else
+                if [ "$(($random_number % 2))" -eq 0 ]; then
+                    if [ "$random_number" -eq 0 ]; then
+                        echo "[+] Ha salido el 0, por tanto has perdido "
+                        initial_bet=$(($initial_bet+1))
+                        bad_games+="$random_number "
+                    else
+                        echo "[+] El número que ha salido es par, pierdes!"
+                        initial_bet=$(($initial_bet+1))
+                        bad_games+="$random_number "
+                    fi
+                else
+                    echo "[+] El número que ha salido es impar, ganas!"
+                    reward=$(($initial_bet*2))
+                    echo "[+] Ganas un total de $reward€"
+                    money=$(($money+$reward))
+                    better_game=$money
+                    if [ $money -gt $better_game ]; then
+                        better_game=$money
+                    fi
+                    if [ $initial_bet == 1 ]; then
+                        initial_bet=1
+                    else
+                        initial_bet=$(($initial_bet-1))
+                    fi
+                    bad_games=" "
+                fi
+                echo -e "[+] Tines $money€\n"
+                let play_counter+=1
+                #sleep 1.5
+            fi
+        else
+            # Nos quedamos sin dinero
+            echo -e "\n${redColour}[!] Te has quedado sin dinero${endColour}\n"
+            echo -e "${redColour}[!] Total de jugadas: $play_counter${endColour}"
+            echo -e "${redColour}[!] Total de jugadas malas: $bad_games${endColour}"
+            echo -e "${redColour}[!] Mayor cantidad de dinero conseguido: $better_game${endColour}"
+            exit 0
+        fi
+    done
+}
+
 while getopts "m:t:h" arg; do 
     case $arg in
         m) money=$OPTARG;;
@@ -249,6 +338,8 @@ if [ $money ] && [ $technique ]; then
     elif [ "$technique" = "fibonacci" ]; then
         fibonacci
 
+    elif [ "$technique" = "alembert" ]; then
+        alembert
     else
         echo -e "\n${redColour}[!] La técnica introducida no existe"
         helpPanel
